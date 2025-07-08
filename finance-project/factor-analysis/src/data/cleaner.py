3e9f94a381b200246afb5fcbc1fd3547901f5566
D.Hwan Kim <131435026+hwan1111@users.noreply.github.com>
2025-07-08 16:52:44 +0900

    Create cleaner.py

---
+++ finance-project/factor-analysis/src/data/cleaner.py
@@ -0,0 +1,15 @@
+import os
+import json
+import pandas as pd
+import numpy as np
+
+from datetime import datetime, timedelta
+from tqdm import tqdm
+
+def drop_nan(df, excluded_tickers_path='excluded_tickers.json'):
+    with open(excluded_tickers_path) as file:
+        excluded_tickers = set(json.load(file)['excluded_tickers'])
+
+    df = df[~df['종목코드'].isin(excluded_tickers)].reset_index(drop=True)
+    df['등락률'] = df['등락률'].fillna(0)
+    return df
