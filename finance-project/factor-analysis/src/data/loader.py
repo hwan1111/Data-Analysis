846a2d45f19b5669f472f63d09f1333bb6082c6c
D.Hwan Kim <131435026+hwan1111@users.noreply.github.com>
2025-07-14 14:03:03 +0900

    Update loader.py
    fix: 전일 기준 최근 개장일만 반영되도록 ohlcv, fundamental, marketcap 수집 함수 수정, update_kospi함수 정렬 수정

--- finance-project/factor-analysis/src/data/loader.py
+++ finance-project/factor-analysis/src/data/loader.py
@@ -12,8 +12,13 @@ from src.data.cleaner import drop_nan
 
 def update_kospi_ohlcv(filepath="data/kospi_ohlcv.parquet"):
     today = datetime.today().date()
+    today_str = today.strftime('%Y%m%d')
+    
+    # 어제를 기준으로 가장 가까운 개장일을 찾는다
     yesterday = today - timedelta(days=1)
     yesterday_str = yesterday.strftime('%Y%m%d')
+    nearest_opening_date_str = pkstock.get_nearest_business_day_in_a_week(yesterday_str, prev=True)
+    nearest_opening_date = datetime.strptime(nearest_opening_date_str, "%Y%m%d").date()
 
     # 기존 데이터 로드
     if os.path.exists(filepath):
@@ -21,8 +26,8 @@ def update_kospi_ohlcv(filepath="data/kospi_ohlcv.parquet"):
         print(f"OHLCV 기존 파일 로드됨: {len(df_all):,} rows")
 
         latest_date = df_all['날짜'].max().date()
-        if latest_date >= yesterday:
-            print("이미 최신 데이터까지 포함되어 있음.")
+        if latest_date >= nearest_opening_date:
+            print(f"{nearest_opening_date}까지 이미 반영되어 있어, 갱신 불필요.")
             return df_all
         start = latest_date + timedelta(days=1)
     else:
@@ -37,20 +42,12 @@ def update_kospi_ohlcv(filepath="data/kospi_ohlcv.parquet"):
 
     for ticker in tqdm(tickers, desc=f"종목별 OHLCV 수집"):
         try:
-            df = pkstock.get_market_ohlcv_by_date(start_str, yesterday_str, ticker).reset_index()
+            df = pkstock.get_market_ohlcv_by_date(start_str, nearest_opening_date_str, ticker).reset_index()
             if df.empty:
                 continue
-
             df['종목코드'] = ticker
             df['종목명'] = pkstock.get_market_ticker_name(ticker)
-
-            df = df[[
-                '날짜',
-                '종목코드',
-                '종목명',
-                '종가'
-            ]]
-
+            df = df[['날짜', '종목코드', '종목명', '종가']]
             all_data.append(df)
         except Exception as e:
             print(f"[{ticker}] 에러 발생: {e}")
@@ -62,62 +59,65 @@ def update_kospi_ohlcv(filepath="data/kospi_ohlcv.parquet"):
     df_new = pd.concat(all_data, ignore_index=True)
     df_new['날짜'] = pd.to_datetime(df_new['날짜'])
 
-    # 기존과 병합 후 중복 제거
     df_all = pd.concat([df_all, df_new], ignore_index=True)
     df_all.drop_duplicates(subset=['날짜', '종목코드'], keep='last', inplace=True)
 
-    # 등락률 재계산
     df_all.sort_values(['종목코드', '날짜'], inplace=True)
     df_all['등락률'] = df_all.groupby('종목코드')['종가'].pct_change() * 100
-
     df_all.sort_index(inplace=True)
 
-    # 저장
     df_all.to_parquet(filepath)
-    print(f"{yesterday_str}까지 반영 완료. 총 {len(df_all):,} rows → {filepath}")
+    print(f"{nearest_opening_date_str}까지 반영 완료. 총 {len(df_all):,} rows → {filepath}")
 
     return df_all
 
 def update_kospi_fundamental(
-        filepath='data/kospi_fundamental.parquet',
-        json_path='data/excluded_tickers.json'):
+    filepath='data/kospi_fundamental.parquet',
+    json_path='data/excluded_tickers.json'):
 
+    # 제외할 종목 코드 로딩
     with open(json_path, 'r') as file:
         EXCLUDED_TICKERS = set(json.load(file)['excluded_tickers'])
 
+    # 오늘 날짜
     today = datetime.today().date()
+    today_str = today.strftime('%Y%m%d')
+
+    # 어제를 기준으로 가장 가까운 개장일을 찾는다
     yesterday = today - timedelta(days=1)
     yesterday_str = yesterday.strftime('%Y%m%d')
+    nearest_opening_date_str = pkstock.get_nearest_business_day_in_a_week(yesterday_str, prev=True)
+    nearest_opening_date = datetime.strptime(nearest_opening_date_str, "%Y%m%d").date()
 
     # 기존 데이터 로드
     if os.path.exists(filepath):
         df_all = pd.read_parquet(filepath)
-        print(f"Fundamental 기존 파일 로드됨: {len(df_all):,} rows")
+        print(f'Fundamental 기존 파일 로드됨: {len(df_all):,} rows')
 
         latest_date = df_all['날짜'].max().date()
-        if latest_date >= yesterday:
-            print('이미 최신 데이터까지 포함되어 있음.')
+        if latest_date >= nearest_opening_date:
+            print(f"{nearest_opening_date}까지 이미 반영되어 있어, 갱신 불필요.")
             return df_all
         start = latest_date + timedelta(days=1)
     else:
         df_all = pd.DataFrame()
         start = today - timedelta(days=365)
-
+    
     start_str = start.strftime('%Y%m%d')
 
     # 종목 리스트
     tickers = pkstock.get_market_ticker_list(market='KOSPI')
     all_data = []
 
-    for ticker in tqdm(tickers, desc=f'종목별 재무재표 수집'):
+    for ticker in tqdm(tickers, desc='종목별 재무재표 수집'):
         try:
             if ticker in EXCLUDED_TICKERS:
                 continue
-
-            df = pkstock.get_market_fundamental_by_date(start_str, yesterday_str, ticker).reset_index()
+            
+            df = pkstock.get_market_fundamental_by_date(start_str, nearest_opening_date_str, ticker).reset_index()
             if df.empty:
                 continue
-
+            
             df['종목코드'] = ticker
             df['종목명'] = pkstock.get_market_ticker_name(ticker)
 
@@ -148,22 +148,27 @@ def update_kospi_fundamental(
     df_all.drop_duplicates(subset=['날짜', '종목코드'], keep='last', inplace=True)
     df_all.to_parquet(filepath)
 
-    print(f"{yesterday_str}까지 반영 완료. 총 {len(df_all):,} rows → {filepath}")
+    print(f"{nearest_opening_date_str}까지 반영 완료. 총 {len(df_all):,} rows → {filepath}")
     return df_all
 
 def update_kospi_marketcap(filepath="data/kospi_marketcap.parquet"):
     today = datetime.today().date()
+    today_str = today.strftime('%Y%m%d')
+    
+    # 어제를 기준으로 가장 가까운 개장일을 찾는다
     yesterday = today - timedelta(days=1)
     yesterday_str = yesterday.strftime('%Y%m%d')
+    nearest_opening_date_str = pkstock.get_nearest_business_day_in_a_week(yesterday_str, prev=True)
+    nearest_opening_date = datetime.strptime(nearest_opening_date_str, "%Y%m%d").date()
 
     # 기존 데이터 로드
     if os.path.exists(filepath):
         df_all = pd.read_parquet(filepath)
-        print(f'MarketCap 기존 파일 로드됨: {len(df_all)}: rows')
+        print(f'MarketCap 기존 파일 로드됨: {len(df_all):,} rows')
 
         latest_date = df_all['날짜'].max().date()
-        if latest_date >= yesterday:
-            print('이미 최신 데이터까지 포함되어 있음')
+        if latest_date >= nearest_opening_date:
+            print(f"{nearest_opening_date}까지 이미 반영되어 있어, 갱신 불필요.")
             return df_all
         start = latest_date + timedelta(days=1)
     else:
@@ -178,7 +183,7 @@ def update_kospi_marketcap(filepath="data/kospi_marketcap.parquet"):
 
     for ticker in tqdm(tickers, desc='종목별 시가총액 수집'):
         try:
-            df = pkstock.get_market_cap_by_date(start_str, yesterday_str, ticker).reset_index()
+            df = pkstock.get_market_cap_by_date(start_str, nearest_opening_date_str, ticker).reset_index()
             if df.empty:
                 continue
 
@@ -193,7 +198,6 @@ def update_kospi_marketcap(filepath="data/kospi_marketcap.parquet"):
                 '거래량',
                 '거래대금'
             ]]
-
             all_data.append(df)
         except Exception as e:
             print(f'[{ticker}] 에러 발생: {e}')
@@ -210,7 +214,7 @@ def update_kospi_marketcap(filepath="data/kospi_marketcap.parquet"):
     df_all.drop_duplicates(subset=['날짜', '종목코드'], keep='last', inplace=True)
     df_all.to_parquet(filepath)
 
-    print(f"{yesterday_str}까지 반영 완료. 총 {len(df_all):,} rows → {filepath}")
+    print(f"{nearest_opening_date_str}까지 반영 완료. 총 {len(df_all):,} rows → {filepath}")
     return df_all
     
 def update_kospi_sector(csv_path='data/업종분류 현황.csv'):
@@ -248,6 +252,8 @@ def update_kospi(clean=True):
     cols = ['날짜', '종목코드', '종목명', '업종명', '종가',
             '등락률', '시가총액', '거래량', '거래대금',
             'BPS', 'PER', 'PBR', 'EPS', 'DIV', 'DPS']
+
+    merge_df.sort_values(['종목명', '날짜'], inplace=True)
 
     result = merge_df[cols]
     if clean is True:
